02989fc52cd09db52875136617602e396325a52a
George Calin <george.calin@lacon.de>
2019-11-20 13:08:47 +0200

    Create new heading style.

--- CSharp Practice OOXML/CSharp Practice OOXML/Program.cs
+++ CSharp Practice OOXML/CSharp Practice OOXML/Program.cs
@@ -16,32 +16,57 @@ namespace CSharp_Practice_OOXML
         static void Main(string[] args)
         {
             /* *** Define names and complete path of the file *** */
-            string completePathToNewFile = "C:\\Test OpenXML\\myNewFile.docx";
-            string completePathToExistingFile = "C:\\Test OpenXML\\myText.docx";
+            string filename = "\\New Word Document With Styles.docx";
+            string path = "C:\\Test OpenXML";
+            string completePathOfNewFile = path + filename;
 
-            /* *** Use Open Office XML to define content *** */
-
-
-            /* *** Use System.IO to copy the existing file *** */
-            if (File.Exists(completePathToNewFile))
+            /* *** Create document *** */
+            using (WordprocessingDocument file = WordprocessingDocument.Create(completePathOfNewFile, WordprocessingDocumentType.Document)) 
             {
-                File.Delete(completePathToNewFile);
+                file.AddMainDocumentPart();
+
+                //Create styles definitions part
+                StyleDefinitionsPart styleDefinitionsPart = file.MainDocumentPart.AddNewPart<StyleDefinitionsPart>();
+
+                //Create styles
+                Styles styles = new Styles();
+                styles.Save(styleDefinitionsPart);
+                styles = styleDefinitionsPart.Styles;
+
+                //Create style
+                Style style = new Style() 
+                { 
+                Type=StyleValues.Paragraph,
+                StyleId="header1",
+                CustomStyle=true,
+                Default=false
+                };
+
+                style.Append(new StyleName() { Val = "Header 1" });
+
+                StyleRunProperties styleRunProperties = new StyleRunProperties();
+                styleRunProperties.Append(new Bold());
+                styleRunProperties.Append(new RunFonts() { Ascii = "Arial" });
+                styleRunProperties.Append(new FontSize() { Val = "24" });
+
+                style.Append(styleRunProperties);
+                styles.Append(style);
+
+                /*** Use Open Office XML to contain content in Word File *** */
+                Text text = new Text("This is text created with the purpose of setting a new style");
+                Run run = new Run(text);
+                Paragraph paragraph = new Paragraph(run);
+                Body body = new Body(paragraph);
+                Document document = new Document(body);
+
+                paragraph.ParagraphProperties = new ParagraphProperties(new ParagraphStyleId() { Val = "header1" });
+
+                file.MainDocumentPart.Document = document;
+                file.MainDocumentPart.Document.Save();
+
+
+
             }
-
-            File.Copy(completePathToExistingFile, completePathToNewFile);
-
-            /* *** Use Open Office XML to open file and add its new content *** */
-            Text text = new Text("This is the text to be added for testing the feature of setting the heading to a paragraph.");
-            Run run = new Run(text);
-            Paragraph paragraph = new Paragraph(run);
-            paragraph.ParagraphProperties = new ParagraphProperties(new ParagraphStyleId() { Val = "Header1" });
-
-            /* *** Use Open Office XML to construct the file *** */
-
-            WordprocessingDocument file = WordprocessingDocument.Open(completePathToNewFile, true);
-
-            file.MainDocumentPart.Document.AppendChild(paragraph);
-            file.MainDocumentPart.Document.Save();
 
         }
     }
