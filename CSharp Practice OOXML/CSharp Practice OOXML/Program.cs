8168ebcfabc591938e459695345de952b0a96833
George Calin <george.calin@lacon.de>
2019-11-20 10:15:57 +0200

    This is the "Hello World" simple example of using OOXML.

---
+++ CSharp Practice OOXML/CSharp Practice OOXML/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+
+namespace CSharp_Practice_OOXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* *** Define the complete path to the file which is about to be constructed *** */
+            string pathToFolder = "C:\\Test OpenXML";
+            string completePathToFile =pathToFolder+ "\\myText.docx";
+
+            /* *** Use Open Office XML to define content *** */
+            Text text = new Text("This is my first Open Office Application.");
+            Run run = new Run(text);
+            Paragraph paragraph = new Paragraph(run);
+            Body body = new Body(paragraph);
+            Document document = new Document(body);
+
+            /* *** Use Open Office XML to build the file *** */
+            using (var file=WordprocessingDocument.Create(completePathToFile,WordprocessingDocumentType.Document))
+            {
+                file.AddMainDocumentPart();
+                file.MainDocumentPart.Document = document;
+                file.MainDocumentPart.Document.Save();
+            }
+        }
+    }
+}
---
+++ CSharp Practice OOXML/CSharp Practice OOXML/Readme.txt
@@ -0,0 +1,3 @@
+﻿Read more about OOXML at
+
+* https://coders-corner.net/2014/12/07/hello-open-xml-world/.
